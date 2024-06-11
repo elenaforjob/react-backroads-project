ab1897b715a82d321aeb2e364abb83b03de603ff
elenaforjob <elenaforjob708@gmail.com>
2024-06-11 21:14:16 +0300

    Tour components plus fix for key on child element

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,9 +7,9 @@ const Services = () => {
     <section className="section services" id="services">
       <Title title="our" subTitle="services" />
       <div className="section-center services-center">
-        {services.map((link) => {
+        {services.map((link, index) => {
           const { icon, title, text } = link;
-          return <Service icon={icon} title={title} text={text} />;
+          return <Service key={index} icon={icon} title={title} text={text} />;
         })}
       </div>
     </section>
--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -7,8 +14,8 @@ export const pageLinks = [
 
 export const socialIcons = [
   { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
-  { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
-  { id: 1, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
 ];
 
 export const services = [
@@ -19,15 +26,78 @@ export const services = [
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
   },
   {
-    id: 1,
+    id: 2,
     icon: "fa-tree",
     title: "endless hiking",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
   },
   {
-    id: 1,
+    id: 3,
     icon: "fa-socks",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    price: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: 68,
+    price: 5100,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: 11,
+    price: 1400,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "kenya",
+    duration: 20,
+    price: 3300,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: "august 26th, 2020",
+    title: "Zanzibar Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Tanzania",
+    duration: 13,
+    price: 3500,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: "august 25th, 2020",
+    title: "Tunisia Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Tunisia",
+    duration: 6,
+    price: 1100,
   },
 ];
